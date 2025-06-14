553d006b31e2ba7a1252d62b1dd95b0a55c8a2c1
Gokul PM <mail.gokulpm@gmail.com>
2025-06-14 05:37:45 +0530

    test: verify

---
+++ verifier_test.go
@@ -0,0 +1,33 @@
+package verifier
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func TestVerify(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.TODO(), 30*time.Second)
+	defer cancel()
+	conf := &Conf{CheckDisposableDomains: true, CheckNS: true}
+	ok, err := Verify(ctx, "mail.gokulpm@gmail.com", conf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !ok {
+		t.Fatal("Verify() returned false, expected true")
+	}
+}
+
+func TestVerifyDisposableDomain(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.TODO(), 30*time.Second)
+	defer cancel()
+	conf := &Conf{CheckDisposableDomains: true, CheckNS: true}
+	ok, err := Verify(ctx, "mail.gokulpm@0uxpgdvol9n.gq", conf)
+	if err != ErrDisposableEmail {
+		t.Fatal(err)
+	}
+	if ok {
+		t.Fatal("Verify() returned true, expected false")
+	}
+}
